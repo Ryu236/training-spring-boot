be97994fd0cbc96ee4afd66d8f55a446acf2fc26
Ryu236 <ryu.adv.2360@gmail.com>
2020-01-13 00:57:15 +0900

    :sparkles: Feat(tutorial): add blog tutorial

--- src/main/kotlin/com/ryu236/trainingspringboot/TrainingSpringBootApplication.kt
+++ src/main/kotlin/com/ryu236/trainingspringboot/TrainingSpringBootApplication.kt
@@ -1,5 +1,6 @@
 package com.ryu236.trainingspringboot
 
+import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,7 @@ import org.springframework.boot.runApplication
 class TrainingSpringBootApplication
 
 fun main(args: Array<String>) {
-	runApplication<TrainingSpringBootApplication>(*args)
+	runApplication<TrainingSpringBootApplication>(*args) {
+		setBannerMode(Banner.Mode.OFF)
+	}
 }
